7b70e50029a5c34abe6ed23ff269e23ba9cee1d7
limaocomvodka <104787948+limaocomvodka@users.noreply.github.com>
2022-07-08 10:34:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+ // Crie um array que receba 5 itens e exiba no console.
+
+ let meninas=["Ana", "Carol","Maria","Luana","Josiane"]
+ console.log(meninas)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+
+meninas.unshift("Paula")
+console.log (meninas)
+// Utilize um método para remover o último nome do array.
+meninas.pop()
+console.log(meninas)
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+meninas.push("Alice", "Thayane")
+console.log (meninas)
+
+// Utilize um método para remover o primeiro nome do array.
+meninas.shift()
+console.log (meninas)
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
